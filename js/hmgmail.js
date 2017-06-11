7196dd353b2674a71b42fb45315c17cbf15d72ef
Pavel <pavel.websbee@gmail.com>
2017-06-11 11:18:57 +0300

    file upload github.com

---
+++ js/hmgmail.js
@@ -0,0 +1,596 @@
+var emails = [{
+    title: "TAB num 1",
+    date: "15.01.17",
+    body: "Hello there!! how are have been doing lately? We have a big surprise for youal",
+    sender: "ad@asdasd.com"
+}, {
+    title: "Miss america!",
+    date: "23.12.16",
+    body: "Is the pritiest wommen alive. And we love here for that. So please keep folowing us",
+    sender: "asdas.jjadsfl@lkfadsflf.com"
+}, {
+    title: "Just bie it!!!",
+    date: "30.11.16",
+    body: "We are seling the stuff you need to bie",
+    sender: "dsfl@lkfalf.com"
+
+}, {
+    title: "Did you get it?!",
+    date: "15.01.17",
+    body: "Hello there!! how are have been doing lately? We have a big surprise for youal",
+    sender: "jjadsfl@lkfadsflf.com"
+}, {
+    title: "Miss america!",
+    date: "23.12.16",
+    body: "Is the pritiest wommen alive. And we love here for that. So please keep folowing us",
+    sender: "asdas.jjadsfl@lkfadsflf.com"
+}, {
+    title: "Just bie it!!!",
+    date: "30.11.16",
+    body: "We are seling the stuff you need to bie",
+    sender: "dsfl@lkfalf.com"
+}, {
+    title: "Did you get it?!",
+    date: "15.01.17",
+    body: "Hello there!! how are have been doing lately? We have a big surprise for youal",
+    sender: "jjadsfl@lkfadsflf.com"
+}, {
+    title: "Miss america!",
+    date: "23.12.16",
+    body: "Is the pritiest wommen alive. And we love here for that. So please keep folowing us",
+    sender: "asdas.jjadsfl@lkfadsflf.com"
+}, {
+    title: "Just bie it!!!",
+    date: "30.11.16",
+    body: "We are seling the stuff you need to bie",
+    sender: "dsfl@lkfalf.com"
+
+}, {
+    title: "Did you get it?!",
+    date: "15.01.17",
+    body: "Hello there!! how are have been doing lately? We have a big surprise for youal",
+    sender: "jjadsfl@lkfadsflf.com"
+}, {
+    title: "Miss america!",
+    date: "23.12.16",
+    body: "Is the pritiest wommen alive. And we love here for that. So please keep folowing us",
+    sender: "asdas.jjadsfl@lkfadsflf.com"
+}, {
+    title: "Just bie it!!!",
+    date: "30.11.16",
+    body: "We are seling the stuff you need to bie",
+    sender: "dsfl@lkfalf.com"
+}, {
+    title: "Did you get it?!",
+    date: "15.01.17",
+    body: "Hello there!! how are have been doing lately? We have a big surprise for youal",
+    sender: "jjadsfl@lkfadsflf.com"
+}, {
+    title: "Miss america!",
+    date: "23.12.16",
+    body: "Is the pritiest wommen alive. And we love here for that. So please keep folowing us",
+    sender: "asdas.jjadsfl@lkfadsflf.com"
+}, {
+    title: "Just bie it!!!",
+    date: "30.11.16",
+    body: "We are seling the stuff you need to bie",
+    sender: "dsfl@lkfalf.com"
+
+}, {
+    title: "Miss america!",
+    date: "23.12.16",
+    body: "Is the pritiest wommen alive. And we love here for that. So please keep folowing us",
+    sender: "asdas.jjadsfl@lkfadsflf.com"
+}, {
+    title: "Just bie it!!!",
+    date: "30.11.16",
+    body: "We are seling the stuff you need to bie",
+    sender: "dsfl@lkfalf.com"
+}, {
+    title: "Did you get it?!",
+    date: "15.01.17",
+    body: "Hello there!! how are have been doing lately? We have a big surprise for youal",
+    sender: "jjadsfl@lkfadsflf.com"
+}, {
+    title: "Miss america!",
+    date: "23.12.16",
+    body: "Is the pritiest wommen alive. And we love here for that. So please keep folowing us",
+    sender: "asdas.jjadsfl@lkfadsflf.com"
+}, {
+    title: "Just bie it!!!",
+    date: "30.11.16",
+    body: "We are seling the stuff you need to bie",
+    sender: "dsfl@lkfalf.com"
+
+}, {
+    title: "Did you get it?!",
+    date: "15.01.17",
+    body: "Hello there!! how are have been doing lately? We have a big surprise for youal",
+    sender: "jjadsfl@lkfadsflf.com"
+}, {
+    title: "Miss america!",
+    date: "23.12.16",
+    body: "Is the pritiest wommen alive. And we love here for that. So please keep folowing us",
+    sender: "asdas.jjadsfl@lkfadsflf.com"
+}, {
+    title: "Just bie it!!!",
+    date: "30.11.16",
+    body: "We are seling the stuff you need to bie",
+    sender: "dsfl@lkfalf.com"
+}, {
+    title: "Did you get it?!",
+    date: "15.01.17",
+    body: "Hello there!! how are have been doing lately? We have a big surprise for youal",
+    sender: "jjadsfl@lkfadsflf.com"
+}, {
+    title: "Miss america!",
+    date: "23.12.16",
+    body: "Is the pritiest wommen alive. And we love here for that. So please keep folowing us",
+    sender: "asdas.jjadsfl@lkfadsflf.com"
+}, {
+    title: "Just bie it!!!",
+    date: "30.11.16",
+    body: "We are seling the stuff you need to bie",
+    sender: "dsfl@lkfalf.com"
+
+}];
+
+var emails2 = [{
+    title: " TAB num 2",
+    date: "15.01.17",
+    body: "Hello there!! how are have been doing lately? We have a big surprise for youal",
+    sender: "ad@asdasd.com"
+}, {
+    title: "Did you get it?!",
+    date: "15.01.17",
+    body: "Hello there!! how are have been doing lately? We have a big surprise for youal",
+    sender: "jjadsfl@lkfadsflf.com"
+}, {
+    title: "Miss america!",
+    date: "23.12.16",
+    body: "Is the pritiest wommen alive. And we love here for that. So please keep folowing us",
+    sender: "asdas.jjadsfl@lkfadsflf.com"
+}, {
+    title: "Just bie it!!!",
+    date: "30.11.16",
+    body: "We are seling the stuff you need to bie",
+    sender: "dsfl@lkfalf.com"
+}, {
+    title: "Did you get it?!",
+    date: "15.01.17",
+    body: "Hello there!! how are have been doing lately? We have a big surprise for youal",
+    sender: "jjadsfl@lkfadsflf.com"
+}];
+
+var emails3 = [{
+    title: "TAB num 3",
+    date: "15.01.17",
+    body: "Hello there!! how are have been doing lately? We have a big surprise for youal",
+    sender: "ad@asdasd.com"
+}, {
+    title: "Did you get it?!",
+    date: "15.01.17",
+    body: "Hello there!! how are have been doing lately? We have a big surprise for youal",
+    sender: "jjadsfl@lkfadsflf.com"
+}, {
+    title: "Miss america!",
+    date: "23.12.16",
+    body: "Is the pritiest wommen alive. And we love here for that. So please keep folowing us",
+    sender: "asdas.jjadsfl@lkfadsflf.com"
+}, {
+    title: "Just bie it!!!",
+    date: "30.11.16",
+    body: "We are seling the stuff you need to bie",
+    sender: "dsfl@lkfalf.com"
+
+}];
+
+
+var newEmailArr = [];
+var emailsContainer = document.createElement('div');
+
+//calls
+
+viewMyEmails(emails);
+
+starSelect();
+
+
+// events
+
+document.getElementById("top2box").addEventListener('click', function (event) {
+    var show = document.getElementById("top2boxM");
+    if (show.style.display == 'none') {
+        show.style.display = 'block';
+    } else {
+        show.style.display = 'none';
+    }
+});
+
+document.getElementById("cbox1").addEventListener('click', function (event) {
+    document.querySelector('input#emailCbox').checked = true;
+});
+
+
+document.getElementById("one").addEventListener('click', function (event) {
+    deleteline();
+    viewMyEmails(emails);
+    this.onclick = toggleClass(this);
+    starSelect();
+});
+
+document.getElementById("indexPrimery").addEventListener('click', function (event) {
+    deleteline();
+    viewMyEmails(emails);
+    var primery = document.getElementById("one");
+    primery.onclick = toggleClass(primery);
+    starSelect();
+});
+
+document.getElementById("two").addEventListener('click', function (event) {
+    deleteline();
+    viewMyEmails(emails2);
+    this.onclick = toggleClass(this);
+    starSelect();
+});
+
+document.getElementById("tree").addEventListener('click', function (event) {
+    deleteline();
+    viewMyEmails(emails3);
+    this.onclick = toggleClass(this);
+    starSelect();
+});
+
+
+document.getElementById('compose').addEventListener('click', createNewEmailForm);
+document.getElementById('sendEmail').addEventListener('click', function (event) {
+    loadSavedDrafts();
+});
+document.getElementById('drafts').addEventListener('click', function (event) {
+    loadSavedDrafts();
+});
+
+// functions
+
+function loadSavedDrafts() {
+
+    //  console.log(localStorage.getItem('form'));
+    if (!localStorage.getItem('form')) {
+        var emailSend = JSON.parse(localStorage.getItem('sendEmailFrom'));
+        if (!emailSend.date) {
+            emailSend.date = `${new Date().getFullYear()}:${new Date().getMonth()+1}:${new Date().getDate()}`;
+        }
+        deleteline();
+        viewSendEmails(emailSend);
+
+        this.onclick = toggleClass(this);
+    }
+}
+
+
+
+function viewSendEmails(arrayOfEmailsOld) {
+    var container = document.querySelector("#container");
+    // for (var i = 0; i < arrayOfEmailsOld.length; i++) {
+
+
+    var email = document.createElement("div");
+    email.classList.add('linediv');
+    email.setAttribute("id", "emailDivLine");
+
+    var input = document.createElement("INPUT");
+    input.setAttribute("type", "checkbox");
+    input.id = 'emailCbox';
+    email.appendChild(input);
+
+    var star = document.createElement("span");
+    star.classList.add('stardiv');
+    star.innerHTML = '&#9733';
+    email.appendChild(star);
+
+    var title = document.createElement('span');
+    title.classList.add('title');
+    title.textContent = arrayOfEmailsOld.subject;
+    email.appendChild(title);
+
+    var body = document.createElement('sapn');
+    body.classList.add('body');
+    body.textContent = arrayOfEmailsOld.body;
+
+    var date = document.createElement('span');
+    date.classList.add('date');
+    date.textContent = arrayOfEmailsOld.date;
+    email.appendChild(date);
+
+    container.appendChild(email);
+}
+
+
+function viewMyEmails(arrayOfEmails) {
+    var container = document.querySelector("#container"); //select div by id container
+    for (var i = 0; i < arrayOfEmails.length; i++) {
+
+
+        var email = document.createElement("div"); // div that holed the all emails line
+        email.classList.add('linediv'); // adds a class name
+        email.setAttribute("id", "emailDivLine"); // makes the id
+
+        var input = document.createElement("INPUT"); //checkbox insert
+        input.type = 'checkbox'; //  setAttribute("type", "checkbox");
+        input.id = 'emailCbox';
+        email.appendChild(input);
+
+        var star = document.createElement("span"); //star insert
+        star.classList.add('stardiv');
+        star.innerHTML = '&#9733';
+        email.appendChild(star);
+
+        var title = document.createElement('span');
+        title.classList.add('title');
+        title.textContent = arrayOfEmails[i].title;
+        email.appendChild(title);
+
+        var body = document.createElement('sapn');
+        body.classList.add('body');
+        body.textContent = arrayOfEmails[i].body;
+        email.appendChild(body);
+
+        var date = document.createElement('span');
+        date.classList.add('date');
+        date.textContent = arrayOfEmails[i].date;
+        email.appendChild(date);
+
+        container.appendChild(email);
+    }
+
+}
+
+function deleteline() {
+    var list = document.getElementById("container");
+    while (list.hasChildNodes()) {
+        list.removeChild(list.firstChild);
+    }
+}
+
+function toggleClass(el) {
+    var kids = document.getElementById('tabs').children;
+    for (var i = 0; i < kids.length - 1; i++) {
+        kids[i].className = "tab";
+    }
+    el.className = "selected";
+}
+
+
+// run for on listner for all the stardivs 
+function starSelect() {
+    var star2 = document.querySelectorAll("span.stardiv");
+    for (var i = 0; i < star2.length; i++) {
+        star2[i].onclick = toggleStar;
+    }
+}
+
+function toggleStar(e) {
+    event.target.classList.toggle("pressed");
+}
+
+
+
+function createNewEmailForm(event) {
+    // var newEmailContainer = document.createElement('div');
+    // newEmailContainer.id = 'newEmailDivCon';
+    var form = document.createElement('form');
+    var label = document.createElement('label');
+    var inputSender = document.createElement('input');
+    form.id = 'newEmailForm';
+    label.id = 'newEmailForm';
+    inputSender.type = 'email';
+    inputSender.name = 'sender';
+    inputSender.required = true;
+    inputSender.pattern = '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$';
+    inputSender.placeholder = 'Sender';
+    label.appendChild(inputSender);
+    form.appendChild(label);
+    // newEmailContainer.appendChild(form);
+
+    label = document.createElement('label');
+    var inputSubject = document.createElement('input');
+    label.id = 'input';
+    inputSubject.type = 'text';
+    inputSubject.name = 'subject';
+    inputSubject.required = true;
+    inputSubject.minlength = 3;
+    inputSubject.placeholder = 'Subject';
+    label.appendChild(inputSubject);
+    form.appendChild(label);
+
+    label = document.createElement('label');
+    label.classList.add('inpuText');
+    var inputBody = document.createElement('textarea');
+    inputBody.id = 'newEmailTexAtrea';
+    inputBody.name = 'body';
+    inputBody.required = true;
+    inputBody.minlength = 13;
+    inputBody.placeholder = 'Body';
+    label.appendChild(inputBody);
+    form.appendChild(label);
+
+    label = document.createElement('label');
+    var inputSubmit = document.createElement('input');
+    inputSubmit.id = 'formSendBtn';
+    inputSubmit.type = 'submit';
+    inputSubmit.value = 'Send';
+    label.appendChild(inputSubmit);
+    form.appendChild(label);
+
+    var closeBtn = document.createElement('button');
+    closeBtn.id = 'x_btn';
+    closeBtn.textContent = '✕';
+    form.appendChild(closeBtn);
+    document.body.appendChild(form);
+
+    inputBody.addEventListener('keypress', function (e) {
+        Keydown(e);
+    });
+
+    form.addEventListener('submit', function (event) {
+        saveFormContent(event.target.parentNode);
+        document.querySelector('form').remove();
+        var draft = {
+            sender: form.sender.value,
+            subject: form.subject.value,
+            body: form.body.value,
+        };
+        var json = JSON.stringify(draft);
+        localStorage.setItem("sendEmailFrom", json);
+        localStorage.removeItem('form');
+
+    });
+
+    if (localStorage.getItem('form')) {
+        loadSavedData(form);
+    }
+
+    closeBtn.addEventListener('click', closeForm);
+
+}
+
+function Keydown(e) {
+    console.log(e);
+    //  if (localStorage.getItem('form')) {
+    saveFormContentKey(e.target.parentNode); // (e.target.parentNode) event:--->  target: button #x_btn --->parentNode
+
+}
+
+function saveFormContentKey(form) {
+    var email = JSON.parse(localStorage.getItem('form'));
+    if (!email) {
+        console.log('no draftform');
+        var draft = {
+            sender: form.querySelector('input[name=sender]').value,
+            subject: form.querySelector('input[name=subject]').value,
+            body: form.getElementById('inpuText').value
+        };
+
+        var json = JSON.stringify(draft);
+        localStorage.setItem("form", json);
+    } else {
+        var draft = {
+            sender: form.sender,
+            subject: form.subject,
+            body: form.body,
+        };
+        var json = JSON.stringify(draft);
+        localStorage.setItem("form", json);
+    }
+}
+
+
+
+function closeForm(e) {
+    console.log(e);
+    saveFormContent(e.target.parentNode); // (e.target.parentNode) event:--->  target: button #x_btn --->parentNode
+    document.querySelector('form').remove();
+    e.target.parentNode.remove(); //removes the HTML form
+}
+
+
+function saveFormContent(form) {
+    var draft = {
+        sender: form.querySelector('input[name=sender]').value,
+        subject: form.querySelector('input[name=subject]').value,
+        body: form.querySelector('textarea').value,
+        date: `${new Date().getFullYear()}:${new Date().getMonth()+1}:${new Date().getDate()}`
+    };
+    var json = JSON.stringify(draft);
+    localStorage.setItem("form", json);
+    return;
+}
+
+function loadSavedData(form) {
+    console.log(localStorage.getItem('form'));
+    if (!localStorage.getItem('form')) {
+        return false;
+    }
+
+    var email = JSON.parse(localStorage.getItem('form'));
+    form.querySelector('input[name=sender]').value = email.sender;
+    form.querySelector('input[name=subject]').value = email.subject;
+    form.querySelector('textarea').value = email.body;
+    return form;
+}
+
+
+function addEmail(e) {
+    e.preventDefault();
+    if (e.isDefaultPrevented()) {
+        console.log(e.isDefaultPrevented()); // default event is prevented
+        console.log(e);
+
+        var email = {
+            sender: form.querySelector('input[name=sender]').value,
+            subject: form.querySelector('input[name=subject]').value,
+            body: form.querySelector('textarea').value,
+            date: `${new Date().getFullYear()}:${new Date().getMonth()+1}:${new Date().getDate()}`
+        };
+        console.log(email);
+        emails.unshift(email);
+        viewMyEmails(email);
+        localStorage.removeItem('form');
+        closeForm('click');
+    } else {
+        console.log(e);
+
+        // var email = {
+        //     title: e.target.querySelector('input[name=sender]').value,
+        //     body: e.target.querySelector('textarea').value,
+        //     date: `${new Date().getFullYear()}:${new Date().getMonth()+1}:${new Date().getDate()}`
+        // };
+        var email = {
+            sender: form.querySelector('input[name=sender]').value,
+            subject: form.querySelector('input[name=subject]').value,
+            body: form.querySelector('textarea').value,
+            date: `${new Date().getFullYear()}:${new Date().getMonth()+1}:${new Date().getDate()}`
+        };
+        console.log(email);
+        emails.unshift(email);
+        viewMyEmails(email);
+        localStorage.removeItem('form');
+        closeForm('click');
+    }
+
+}
+
+
+
+
+
+
+
+//   if (this.className == "selected") {
+//         this.classList.remove("selected");
+//     } else {
+//         this.className = "selected";
+//     }
+//     this.classList.toggle('selected');
+
+
+// x.addEventListener("click", mySecondFunction);
+// x.addEventListener("mouseout", myThirdFunction);
+
+
+
+// (function goTrueemails() {
+
+//     for (var i = 0; i < emails.length; i++) {
+//         console.log(emails[i].body);
+//         console.log(emails[i].date);
+//         console.log(emails[i].body);
+//         console.log(emails[i].email);
+//     }
+// })();
+
+// console.log(emails[1].body.slice(0, 25) + "...");
+// console.log(emails[1].body.substring(0, 25) + "...");
+
+// console.log(Object.keys(emails[1]));
+// console.log(Object.values(emails[2]));
